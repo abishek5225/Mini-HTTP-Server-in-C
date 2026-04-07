7d243cf820f522cd00108e3cf3f2d31dcd06765d
Abishek Chaulagain <abishekchaulagain5225@gmail.com>
2026-04-07 17:59:06 +0545

    first commit

---
+++ server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main(){
+        int server_fd, client_socket;
+
+}
